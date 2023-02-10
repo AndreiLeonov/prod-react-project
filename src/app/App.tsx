aa7f48abc730bdeca690987b7874c1f50ad44b1a
AndreiLeonov <andrei_leonov93@mail.ru>
2023-02-10 03:15:32 +0300

    Navbar and ThemeSwitcher added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,16 @@
-import React, {Suspense} from 'react';
 import './styles/index.scss';
-import {Route, Routes} from "react-router-dom";
-import {Link} from 'react-router-dom';
 import {useTheme} from './providers/ThemeProvider';
-import {AboutPage} from 'pages/AboutPage';
-import {MainPage} from "pages/MainPage";
 import {classNames} from 'shared/lib/classNames/classNames';
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
 
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme, 'cls2', 'cls3'])}>
-            <button onClick={toggleTheme}>TOGGLE THEME</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О нас</Link>
+            <Navbar/>
             <AppRouter/>
         </div>
     );
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,10 +1,10 @@
 
 type ModsType = Record<string, boolean | string>
 
-export function classNames(cls: string, mods: ModsType, additional: string[]): string {
+export function classNames(cls: string, mods: ModsType = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods).filter(([className, value]) => Boolean(value)).map(([className, value]) => className)
     ].join(' ')
 }
