02bea1d90676fecfe9a283b3f70c347da32363fb
AndreiLeonov <andrei_leonov93@mail.ru>
2023-02-13 18:19:45 +0300

    added jest test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,15 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional class', () => {
+        const expectedResult = 'someClass cls1 cls2';
+        expect(classNames('someClass', {}, ['cls1', 'cls2'])).toBe(expectedResult);
+    });
+    test('with mods class', () => {
+        const expectedResult = 'someClass cls1 cls2 isTest isTest3';
+        expect(classNames('someClass', { isTest: true, isTest2: false, isTest3: true }, ['cls1', 'cls2'])).toBe(expectedResult);
+    });
+});
