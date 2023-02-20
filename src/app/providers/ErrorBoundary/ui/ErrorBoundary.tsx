7a84f87167f753fc2fb94794a608eb7a5f51a780
AndreiLeonov <andrei_leonov93@mail.ru>
2023-02-21 01:05:34 +0300

    added PageError and ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,45 @@
+import React, { ErrorInfo, ReactNode, Suspense } from 'react';
+import { PageError } from 'widgets/PageError/ui/PageError';
+
+interface IErrorBoundaryProps {
+    children: ReactNode;
+}
+
+interface IErrorBoundaryState {
+    hasError: boolean;
+}
+
+// eslint-disable-next-line max-len
+class ErrorBoundary extends React.Component<IErrorBoundaryProps, IErrorBoundaryState> {
+    constructor(props: IErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+        // Обновить состояние с тем, чтобы следующий рендер показал запасной UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        // Можно также сохранить информацию об ошибке в соответствующую службу журнала ошибок
+        console.log(error, errorInfo);
+    }
+
+    render() {
+        const { hasError } = this.state;
+        const { children } = this.props;
+        if (hasError) {
+            // Можно отрендерить запасной UI произвольного вида
+            return (
+                <Suspense fallback="">
+                    <PageError />
+                </Suspense>
+            );
+        }
+
+        return children;
+    }
+}
+
+export default ErrorBoundary;
